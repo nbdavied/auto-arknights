f1aeae90b35772b5ecd9f7071372fa82649fd73a
dai-surface <nbdavied@gmail.com>
2019-12-29 13:18:23 +0800

    增加氪石头功能 战斗过程中通过ocr识别进度，调整sleep时间 点击坐标通过随机函数添加偏移

--- auto_game.py
+++ auto_game.py
@@ -5,6 +5,9 @@
 
 import os,time
 import cv2
+import random
+import pytesseract
+from PIL import Image
 
 def connect():
     try:
@@ -12,7 +15,22 @@ def connect():
     except:
         print('连接失败')
 
-def click(x, y):
+def click(center):
+    x = center[0]
+    y = center[1]
+    pn_x = random.random()
+    pn_y = random.random()
+    dis_x = random.random() * center[2]
+    dis_y = random.random() * center[3]
+    if(pn_x < 0.5):
+        x = x - dis_x
+    else:
+        x = x + dis_x
+    if(pn_y < 0.5):
+        y = y - dis_y
+    else:
+        y = y + dis_y
+    print('x:',x, 'y:', y)
     os.system('adb shell input tap %s %s' % (x, y))
 
 def screenshot():
@@ -31,45 +49,112 @@ def resize_img(img_path):
 def Image_to_position(image, m = 0):
     image_path = 'images/' + str(image) + '.png'
     screen = cv2.imread('images/screen.png', 0)
-    # template = cv2.imread(image_path, 0)
-    template = resize_img(image_path)
+    if image == 'stone':
+        template = cv2.imread(image_path, 0)
+    else:
+        template = resize_img(image_path)
     methods = [cv2.TM_CCOEFF_NORMED, cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED]
-    image_x, image_y = template.shape[:2]
+    image_y, image_x = template.shape[:2]
     result = cv2.matchTemplate(screen, template, methods[m])
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     # print(max_val)
-    if max_val > 0.8:
+    MIN = 0.7
+    # if image =='stone':
+    #     MIN = 0.6
+    if max_val > MIN:
         global center
-        center = (max_loc[0] + image_y / 2, max_loc[1] + image_x / 2)
+        center = (max_loc[0] + image_x / 2, max_loc[1] + image_y / 2, image_x/2, image_y/2)
         print(center)
         return center
     else:
         return False
-    
+def revertColor(img):
+    # 灰度图片反色，处理成黑白两色
+    x, y = img.shape
+    for i in range(x):
+        for j in range(y):
+            if img[i,j] > 128:
+                img[i,j] = 0
+            else:
+                img[i,j] = 255
+    return img
+
+def enlarge(img):
+    # 图片外围填充白色边控
+    # 不然tesseract无法识别
+    largepic = cv2.copyMakeBorder(img, 30,30,30,30,cv2.BORDER_CONSTANT,value = [255,255,255])
+    return largepic
+
+def ocrRead(img):
+    # 读取图片中的文字
+    text = pytesseract.image_to_string(img)
+    print(text)
+    return text
+
+def read_battle_progress():
+    screen = cv2.imread('images/screen.png', 0)
+    progressimg = screen[20:70,660:770]
+    progressimg = revertColor(progressimg)
+    cv2.imwrite('images/tmp.png',progressimg)
+    progressimg = cv2.imread('images/tmp.png')
+    progressimg = enlarge(progressimg)
+    progressimg = Image.fromarray(cv2.cvtColor(progressimg, cv2.COLOR_BGR2RGB))
+    text = ocrRead(progressimg)
+    return text
+
+def sleepAccordingProgress(progress):
+    if progress == '':
+        time.sleep(5)
+        return
+    kill,all = progress.split('/')
+    kill = int(kill)
+    all = int(all)
+    rest = all - kill
+    sleeptime = rest / all * 60
+    if sleeptime < 2:
+        sleeptime = 2
+    print('sleep ', sleeptime, ' seconds')
+    time.sleep(sleeptime)
+
 def run(n):
-    images = ['start-go1', 'start-go2', 'end', 'level up']
+    images = {
+        'unknown':['start-go1', 'start-go2', 'end', 'level up', 'stone'],
+        'start-go1':['start-go2','stone'],
+        'start-go2':['end','level up'],
+        'end':['start-go1','level up'],
+        'stone':['start-go1']
+        }
     round = 0
     # Image_to_position('start-go1')
     # time.sleep(2)
     # Image_to_position('start-go2')
     # while not Image_to_position('end'):
     #     time.sleep(5)
+    current='unknown'
     while True:
         screenshot()
         now = ''
-        for image in images:
+        for image in images[current]:
             if Image_to_position(image, m = 0) != False:
                 print(image)
                 now = image
+                current=image
                 time.sleep(0.5)
-                click(center[0], center[1])
+                click(center)
+                break
                 
         if now == 'end':
             time.sleep(0.8)
             round = round + 1
+            print('打完第%i/%i次'%(round,n))
             if round == n:
                 break
-        
+        if now == 'stone':
+            time.sleep(1.5)
+        if current == 'start-go2':
+            print('ready for ocr')
+            p = read_battle_progress()
+            sleepAccordingProgress(p)
 
 if __name__ == '__main__':
     connect()
